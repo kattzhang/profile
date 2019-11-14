3cebc79c5b25d1e6d242415a8dc15f1eda9b79c7
zhangchengxin <zhangchengxin@meicai.cn>
2019-11-14 19:54:09 +0800

    update release.sh

--- build/release.sh
+++ build/release.sh
@@ -3,10 +3,16 @@
 # immediatelly exit if any command has a no-zero exit code
 set -e
 
+# commit
+git checkout dev
 git add -A
 git commit -m '[build]'
+
+# merge
 git checkout master
 git merge dev
+
+# push
 git push origin master
 git checkout dev
 git rebase master
