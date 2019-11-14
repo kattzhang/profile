78213f1676baae1389f98382bf05aa34cb7f75a3
zhangchengxin <zhangchengxin@meicai.cn>
2019-11-14 19:36:50 +0800

    update release.sh

--- build/release.sh
+++ build/release.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env sh
 
-git checkout master
-git merge dev
+set -e
 
 read -p "Releasing - are you sure? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]   # =~ regular expression match
 then
   echo "Releasing..."
-
-  # commit
-  git add -A
-  git commit -m "[build]"
   
   # push
+  git checkout master
+  git merge dev
   git push origin master
   git checkout dev
   git rebase master
